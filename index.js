5edadb400ac8dea586c08efe955461b6f602fdf6
Yiting Zhang <zhangyiting.yiting@outlook.com>
2025-04-21 17:38:34 +0200

    fix number 6 and 7.

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
-while (running){
-  const choice = prompt(`
-    Book Tracker
-    1. Add Book
-    2. List Books
-    3. Mark Book as Read
-    4. Exit
-    Enter your choice:`);
+let running = true;
 
-  switch (choice) {
-    case "1":
-      addBook();
-      break;
-    case "2":
-      listBooks();
-      break;
-    case "3":
-      const title = prompt("Enter the title of the book to mark as read:")
-      markAsRead(title);
-      break
-    case "4":
-    running = false;
-    alert("Goodbye!");
-    break;
-    default:
-      alert("Invalid choice.");
-      break;
-  }
-}
-
-
-const book = {
-  title: "The power of now",
-  author: "Eckhart Tolle",
-  isRead: false
-}
 
 const library = [
   {
@@ -47,6 +13,7 @@ const library = [
     isRead: false
   }
 ];  
+
 
 function addBook() {
   let title = prompt("Enter the book's name");
@@ -78,16 +45,24 @@ function addBook() {
 
   library.push(newBook);
   alert("Book added successfully!");
+
 }
 
-function listBooks() {
-  library.forEach(function(book) {
-    console.log("Title: " + book.title);
-    console.log("Author: " + book.author);
-    console.log("Read: " + (book.isRead ? "Yes" : "No"));
-    console.log("---------------");
-  });
-}
+
+
+
+
+  function listBooks() {
+    library.forEach(function(book) {
+      console.log("Title: " + book.title);
+      console.log("Author: " + book.author);
+      console.log("Read: " + (book.isRead ? "Yes" : "No"));
+      console.log("---------------");
+    });
+  }
+
+
+
 
 
 function markAsRead(title) {
